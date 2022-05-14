285b64c6e1416d3440d2d91daad3239e08ffbe11
Omkar M <momkar@zeomega.com>
2022-05-14 19:40:38 +0530

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LaunchComponent } from './launch/launch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: 'launch', pathMatch: 'full' },
+  { path: "launch", component: LaunchComponent },
+  { path: "home", component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
