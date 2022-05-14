897c25b5c826aca98f8da26ca707b9de847064a5
Omkar M <momkar@zeomega.com>
2022-05-14 19:46:54 +0530

    Added fhirclient and authorize method

--- src/app/launch/launch.component.ts
+++ src/app/launch/launch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { oauth2 as SMART } from 'fhirclient';
 
 @Component({
   selector: 'app-launch',
@@ -10,6 +11,12 @@ export class LaunchComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    SMART.authorize({
+      iss: 'https://fhir-ehr-code.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d',
+      client_id: '5849c0dc-67ed-44c8-8469-fd7813e7eb0c',
+      scope: 'user/Patient.read user/Coverage.read user/Practitioner.read user/ServiceRequest.read patient/Coverage.read',
+      redirect_uri: 'https://luminous-eclair-320f36.netlify.app/launch',
+    })
   }
 
 }
