a85d67e20b38515261bfe0e1c87ba117bacf6e8d
Omkar M <momkar@zeomega.com>
2022-05-20 19:26:26 +0530

    Added _router in launch

--- src/app/launch/launch.component.ts
+++ src/app/launch/launch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { oauth2 as SMART } from 'fhirclient';
 
 @Component({
@@ -8,11 +9,14 @@ import { oauth2 as SMART } from 'fhirclient';
 })
 export class LaunchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
     console.log("inside launch")
-    debugger
+
+    console.log(this._router.url);
+    console.log(this.getUrlVars(this._router.url))
+
     SMART.authorize({
       iss: 'https://fhir-ehr-code.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d',
       // fhirServiceUrl: 'https://fhir-ehr-code.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d',
@@ -20,8 +24,17 @@ export class LaunchComponent implements OnInit {
       scope: 'launch online_access openid profile user/AllergyIntolerance.read user/Coverage.read user/Observation.read user/Patient.read user/Practitioner.read user/ServiceRequest.read',
       redirect_uri: 'https://luminous-eclair-320f36.netlify.app/home',
     })
+  }
 
-
+  getUrlVars(url: any) {
+    var hash: any;
+    var myJson: any = {};
+    var hashes = url.slice(url.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split('=');
+      myJson[hash[0]] = hash[1];
+    }
+    return myJson;
   }
 
 }
